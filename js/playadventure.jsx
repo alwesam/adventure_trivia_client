038d346ee1ff56549a19edb67bcd44a25f1e0f28
alwesam <alwesam@gmail.com>
2015-08-05 17:36:17 -0700

    fixed url

--- js/playadventure.jsx
+++ js/playadventure.jsx
@@ -26,7 +26,7 @@ var PlayAdventure = React.createClass({
     //questions and riddles
     //pass the token as well
     $.ajax({
-      url: "http://stark-ridge-5017.herokuapp.com/adventures/id="+this.props.adventure_id+"&token="+this.props.adventure_token+".json",
+      url: "http://stark-ridge-5017.herokuapp.com/adventures/"+this.props.adventure_id+"?token="+this.props.adventure_token,
       method: "GET",
       success: function(data){
         console.log("here are the details of adventure >>>>>>>>>>>>>>>>>");
